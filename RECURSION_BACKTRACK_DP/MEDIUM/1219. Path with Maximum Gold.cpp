d96e14a94ac11eae3c2e63ab570aed0e763c0282
Prajwal Karale <54241917+PrajwalKarale@users.noreply.github.com>
2024-05-14 15:06:33 -0400

    Create 1219. Path with Maximum Gold.cpp

---
+++ RECURSION_BACKTRACK_DP/MEDIUM/1219. Path with Maximum Gold.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>>directions = {{-1,0}, {1,0}, {0, 1}, {0,-1}};
+    int dfs(vector<vector<int>> &grid, int i, int j, int row, int column){
+        if(i >= row || i < 0 || j >= column || j < 0 || grid[i][j] == 0){
+            return 0;
+        }
+        // Since it will be useful during backtracking;
+        int ogValue = grid[i][j];
+        grid[i][j] = 0;
+        int maxGold = 0;
+        for(vector<int> &dir: directions){
+            int newX = i + dir[0];
+            int newY = j + dir[1];
+
+            maxGold = max(maxGold, dfs(grid, newX, newY, row, column));
+        }
+
+        grid[i][j] = ogValue;
+        return ogValue + maxGold;
+    }
+    int getMaximumGold(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int column = grid[0].size();
+        int maximumGold = 0;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < column; j++){
+                if(grid[i][j] != 0){
+                    maximumGold = max(maximumGold, dfs(grid, i, j, row, column));
+                }
+            }
+        }
+        return maximumGold;
+    }
+};

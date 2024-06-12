0655d75c40d1d9b3f43b6708d68e74cd2f08c795
Prajwal Karale <54241917+PrajwalKarale@users.noreply.github.com>
2024-06-11 20:27:27 -0400

    Create 75. Sort Colors.cpp

---
+++ BINARY SEARCH/MEDIUM/75. Sort Colors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else{
+                int temp1 = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp1;
+                high--;
+            }
+        }
+    }
+};

b1f8e7d1734bbc81f8b9760057eaf96c38e2a69d
Prajwal Karale <54241917+PrajwalKarale@users.noreply.github.com>
2024-08-06 13:16:14 -0700

    Create 3016. Minimum Number of Pushes to Type Word II.cpp

---
+++ GREEDY/MEDIUM/3016. Minimum Number of Pushes to Type Word II.cpp
@@ -0,0 +1,20 @@
+#include<execution>
+class Solution {
+public:
+    int minimumPushes(string word) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        int result = 0;
+        vector<int>freq(26, 0);
+        for(char &c: word){
+            freq[c - 'a']++;
+        }
+        sort(execution::par_unseq, freq.begin(), freq.end(), greater{});
+        for(int i = 0; i < 26; i++){
+            int numberOfPress = i / 8 + 1;
+            result = result + (numberOfPress * freq[i]);
+        }
+        return result;
+    }
+};

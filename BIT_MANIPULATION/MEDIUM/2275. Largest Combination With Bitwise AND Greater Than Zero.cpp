a3e92360d74d0b02adfba825739debc07960f96f
Prajwal Karale <54241917+PrajwalKarale@users.noreply.github.com>
2024-11-07 21:51:19 -0800

    Update 2275. Largest Combination With Bitwise AND Greater Than Zero.cpp

--- BIT_MANIPULATION/MEDIUM/2275. Largest Combination With Bitwise AND Greater Than Zero.cpp
+++ BIT_MANIPULATION/MEDIUM/2275. Largest Combination With Bitwise AND Greater Than Zero.cpp
@@ -1,3 +1,12 @@
+/*
+    Time Complexity:
+        1. T.C -> O(n) where n = candidates.size
+        2. Since we will be traversing each and every element in the vector
+    Space Complexity:
+        1. S.C -> O(1)
+    Important
+        1. Number of bits required to present a number m -> [floor($log{_2}{n}$)] +1
+*/
 auto fastIO = [](){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
